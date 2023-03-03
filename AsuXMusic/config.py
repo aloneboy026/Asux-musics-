bf8513c14dd20cb3cdbd816d9a98353f188ad453
aloneboy026 <122468535+aloneboy026@users.noreply.github.com>
2023-03-03 23:12:57 +0530

    Update config.py

--- AsuXMusic/config.py
+++ AsuXMusic/config.py
@@ -17,12 +17,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5613528193").split()))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5613528193").split()))
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "600"))
 
-IMG_1 = getenv("IMG_1", "https://te.legra.ph/file/31e9ecee16a46575267a4.jpg")
-IMG_2 = getenv("IMG_2", "https://te.legra.ph/file/d744707634106cf76627a.jpg")
-IMG_3 = getenv("IMG_3", "https://telegra.ph/file/f02efde766160d3ff52d6.png")
-IMG_4 = getenv("IMG_4", "https://telegra.ph/file/be5f551acb116292d15ec.png")
-IMG_5 = getenv("IMG_5", "https://telegra.ph/file/c3401a572375b569138c3.png")
-IMG_6 = getenv("IMG_6", "https://telegra.ph/file/d8f8fc1de9110b93ca94c.jpg")
+IMG_1 = getenv("IMG_1", "https://telegra.ph/file/202c8e5a57f1f8597fe2a.jpg")
+IMG_2 = getenv("IMG_2", "https://telegra.ph/file/53112d2692ac8d0b499c0.jpg")
+IMG_3 = getenv("IMG_3", "https://te.legra.ph/file/f13721b7511585e52a179.jpg")
+IMG_4 = getenv("IMG_4", "https://te.legra.ph/file/ae3abdcc33afe6822c61b.jpg")
+IMG_5 = getenv("IMG_5", "https://te.legra.ph/file/e8007311f301c0011b743.jpg")
+IMG_6 = getenv("IMG_6", "https://te.legra.ph/file/d773ecc7a29e068c9ec3a.jpg")
 YOUTUBE_IMG_URL = getenv(
-    "YOUTUBE_IMG_URL", "https://te.legra.ph/file/bc5556476395a0c8e109b.jpg"
+    "YOUTUBE_IMG_URL", "https://te.legra.ph/file/485f912bde710ba31817d.jpg"
 )
